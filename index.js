72266979351f8c12f961480e1f104be8d188c8b0
Holly Bedford <holly.bedford02@gmail.com>
2020-12-04 11:16:44 +0000

    added notes

--- index.js
+++ index.js
@@ -4,10 +4,13 @@ const mongoClient = require('mongodb').MongoClient;
 const app = express();
 const port = 3003;
 
+//gotta setup bodyparser before trying to post so it knows what to expect
 app.use(bodyP.json());
 app.use(bodyP.urlencoded({extended: false}));
+
 const url = "mongodb://root:password@localhost:27017";
 
+//function to add new accounts to db
 let insertDataInDB = (db, dataToSend) => {
     let collection = db.collection('accounts');
     collection.insertOne(dataToSend);
@@ -18,7 +21,8 @@ let insertDataInDB = (db, dataToSend) => {
 
 
 
-
+//route to post new accounts to
+//POST localhost:x/accounts - body - x-www... - key/value - send
 app.post('/accounts', (req, res) => {
     let newAccHolder = req.body.accholder;
     let newAccName = req.body.accname;
@@ -40,12 +44,8 @@ app.post('/accounts', (req, res) => {
     res.send('inserted');
 })
 
-
-app.get('/', (req, res) => {
-    res.send('hi');
-})
-
-app.listen(port, ()=>{
-    console.log(`yeet on http://localhost:${port}`);
-})
+//home route of nada
+app.get('/', (req, res) => { res.send('hi') })
+//useful thing
+app.listen(port, ()=>{ console.log(`yeet on http://localhost:${port}`) })
 
