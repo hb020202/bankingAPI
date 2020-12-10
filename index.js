3f1d0e0e9ba007de837b2b9b1137f53a6702b531
Holly Bedford <holly.bedford02@gmail.com>
2020-12-10 15:40:00 +0000

    add get route

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const bodyP = require('body-parser');
 const mconnection = require('./mongoconn');
+const cors = require('cors');
 const app = express();
 const port = 3003;
+const mongoClient = require('mongodb').MongoClient;
+
 
 //gotta setup bodyparser before trying to post so it knows what to expect
 app.use(bodyP.json());
 app.use(bodyP.urlencoded({extended: false}));
+app.use(cors());
 
 //mongo URL
 const url = "mongodb://root:password@localhost:27017";
@@ -25,6 +29,10 @@ let updateBalance = (db, dataToUpdate) => {
 let addBalance = (db, addToBalance) => {
     let collection = db.collection('accounts');
     let newBal = addToBalance.addedBal-0;
+    collection.updateOne({accholder: addToBalance.accHolder}, {$inc: {balance: newBal }})
+}//function to show all
+let showAll = (db) => {
+    let collection = db.collection('accounts');
     collection.updateOne({accholder: addToBalance.accHolder}, {$inc: {balance: newBal }})
 }
 
@@ -69,6 +77,24 @@ app.put('/accounts/add', (req, res) => {
     mconnection(url, addBalance, addToBalance);
     res.send('added');
 })
+
+//route to get all accounts from
+//GET localhost:x/viewall - body - x-www... - key/value - send
+app.get('/viewall', (req, res) => {
+    mongoClient.connect(url,
+        {useNewUrlParser: true, useUnifiedTopology:true},
+        (error, client) => {
+            let db = client.db('bankstuff');
+            db.collection('accounts').find().toArray()
+            .then(results => {
+                res.send(results)
+            })
+
+            console.log('connected');
+        })})
+
+
+
 
 
 //home route of nada
